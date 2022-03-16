7c853899cb2109bf85397e86043b60c0ccb3ac28
Héctor Fierros <hfierros@gmail.com>
2022-03-16 17:32:25 -0600

    Add angle example

---
+++ angle/src/main/java/iteso/qaclass/angle/AngleApplication.java
@@ -0,0 +1,13 @@
+package iteso.qaclass.angle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngleApplication.class, args);
+	}
+
+}
---
+++ angle/src/test/java/iteso/qaclass/angle/AngleApplicationTests.java
@@ -0,0 +1,13 @@
+package iteso.qaclass.angle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
